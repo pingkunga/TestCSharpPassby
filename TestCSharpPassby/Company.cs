b2d7b69a00ac4a8091a4d496974cb05f6f99d0a1
pingkunga <debuggingsoft@gmail.com>
2016-07-19 09:29:04 +0700

    Test C# Pass by

---
+++ TestCSharpPassby/Company.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TestCSharpPassby.Properties
+{
+  public class Company
+  {
+     public String CompanyName { get; set; }
+  }
+}
---
+++ TestCSharpPassby/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using TestCSharpPassby.Properties;
+
+namespace TestCSharpPassby
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      Company company = new Company();
+      company.CompanyName= "COMSWU";
+      Console.WriteLine("First >> " + company.CompanyName);
+      ChangeNameVer1(company);
+      Console.WriteLine("Second >> " + company.CompanyName);
+      ChangeNameVer2(company);
+      Console.WriteLine("Third >> " + company.CompanyName);
+      ChangeNameVer3(company);
+      Console.WriteLine("Fourth >> " + company.CompanyName);
+      //ChangeNameVer4(ref company);
+      //Console.WriteLine("Fifth >> " + company.CompanyName);
+      //ChangeNameVer5(ref company);
+      //Console.WriteLine("Sixth >> " + company.CompanyName);
+    }
+
+    public static void ChangeNameVer1(Company pCompany)
+    {
+      pCompany.CompanyName = "DebuggingSoft";
+    }
+
+    public static void ChangeNameVer2(Company pCompany)
+    {
+      pCompany = new Company();
+      pCompany.CompanyName = "Chatri Soft";
+    }
+
+    public static void ChangeNameVer3(Company pCompany)
+    {
+      pCompany = null;
+    }
+
+    public static void ChangeNameVer4(ref Company pCompany)
+    {
+      pCompany = new Company();
+      pCompany.CompanyName = "Chatri Soft";
+    }
+
+    public static void ChangeNameVer5(ref Company pCompany)
+    {
+      pCompany = null;
+    }
+  }
+}
